c2a265461204680f450dfa4fbac4810de4c8ecd8
Gabriel Rezende <gabrielrezendee03@gmail.com>
2026-04-07 16:29:32 -0300

    feat(week-2): add CS50P string and condition exercises
    Add Week 2 solutions (camel, coke, nutrition, plates, twttr) and lecture note examples under notes/.

---
+++ week 2/coke.py
@@ -0,0 +1,17 @@
+# Suppose that a machine sells bottles of Coca-Cola (Coke) for 50 cents and only accepts coins in these denominations: 25 cents, 10 cents, and 5 cents.
+
+# In a file called coke.py, implement a program that prompts the user to insert a coin, one at a time, each time informing the user of the amount due. Once the user has inputted at least 50 cents, output how many cents in change the user is owed. Assume that the user will only input integers, and ignore any integer that isn’t an accepted denomination.
+
+
+coins = [25, 10, 5]
+amount = 50
+
+
+while amount > 0:
+    print(f"Amount Due: {amount}")
+    coin = int(input('Insert Coin: '))
+    if coin in coins:
+        amount -= coin
+
+print(f"Change Owed: {amount * -1}")
+        
